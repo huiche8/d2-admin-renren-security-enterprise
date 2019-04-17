8b892e75a643a6f0f22ff2c0e1b023a31bdc30df
liyang <1711467488@qq.com>
2019-04-17 22:36:37 +0800

    由于人人后台接口返回了 eval 代码，只能使用 window.SITE_CONFIG 的方式

--- src/store/index.js
+++ src/store/index.js
@@ -2,13 +2,11 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 
 import d2admin from './modules/d2admin'
-import renren from './modules/renren'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   modules: {
-    d2admin,
-    renren
+    d2admin
   }
 })
